4e6c76d8b6293fb4c74789577ad94c7440ff2770
ganchuanpu <429533813@qq.com>
2018-07-23 21:23:21 +0800

    发件箱模块

--- app/src/main/java/com/goldenapple/lottery/fragment/MessageBoxFragment.java
+++ app/src/main/java/com/goldenapple/lottery/fragment/MessageBoxFragment.java
@@ -3,6 +3,7 @@ package com.goldenapple.lottery.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +13,28 @@ import android.widget.TextView;
 
 import com.goldenapple.lottery.R;
 import com.goldenapple.lottery.app.BaseFragment;
+import com.goldenapple.lottery.base.net.RestCallback;
+import com.goldenapple.lottery.base.net.RestRequest;
+import com.goldenapple.lottery.base.net.RestRequestManager;
+import com.goldenapple.lottery.base.net.RestResponse;
+import com.goldenapple.lottery.component.CustomDialog;
+import com.goldenapple.lottery.data.ReceiveBoxCommand;
+import com.goldenapple.lottery.data.ReceiveBoxResponse;
+import com.goldenapple.lottery.game.PromptManager;
+import com.goldenapple.lottery.material.ConstantInformation;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import q.rorbin.badgeview.QBadgeView;
 
 
 public class MessageBoxFragment extends BaseFragment {
     private static final String TAG = MessageBoxFragment.class.getSimpleName();
 
-    private int RECEIVE = 0;
-
+    private int LIST = 0;
     @BindView(R.id.in_box_fragment)
     RelativeLayout in_box_fragment;
 
@@ -55,41 +68,22 @@ public class MessageBoxFragment extends BaseFragment {
 
     }
 
-//    private void loadReceiveBox() {
-//        ReceiveBoxUnReadCommand command = new ReceiveBoxUnReadCommand();
-//        command.setIsRead(0);
-//        TypeToken typeToken = new TypeToken<RestResponse<ReceiveBoxResponse>>() {
-//        };
-//
-//        RestRequestManager.executeCommand(getActivity(), command, typeToken, restCallback, RECEIVE, this);
-//    }
+    private void loadReceiveBox() {
+        ReceiveBoxCommand command = new ReceiveBoxCommand();
+        command.setPage(1);
+
+        TypeToken typeToken = new TypeToken<RestResponse<ArrayList<ReceiveBoxResponse>>>() {
+        };
+        RestRequestManager.executeCommand(getActivity(), command, typeToken, restCallback, LIST, this);
+    }
 
     @Override
     public void onResume() {
         Log.i(TAG, "onResume");
         super.onResume();
-//        if( SharedPreferencesUtils.getBoolean(getActivity(), ConstantInformation.APP_INFO,MAIL)) {
-//            int  totalCount=ConstantInformation.MESSAGE_COUNT;
-//
-//            Object  tag=in_box_text.getTag();
-//            if(tag==null){
-//                QBadgeView qBadgeView=new QBadgeView(getActivity());
-//                qBadgeView.bindTarget(in_box_badge);
-//                qBadgeView.setBadgeGravity(Gravity.START | Gravity.TOP);
-//                qBadgeView.setBadgeNumber(totalCount);
-//                in_box_text.setTag(qBadgeView);
-//            }else{
-//                QBadgeView qQBadgeView=(QBadgeView)tag;
-//                qQBadgeView.setBadgeNumber(totalCount);
-//            }
-//        }else{
-//            Object  tag=in_box_text.getTag();
-//
-//            if(tag!=null){
-//                QBadgeView qQBadgeView=(QBadgeView)tag;
-//                qQBadgeView.setBadgeNumber(0);
-//            }
-//        }
+        if(ConstantInformation.MESSAGE_COUNT==-1){
+            loadReceiveBox();
+        }
     }
 
     @OnClick({R.id.in_box_fragment, R.id.out_box_fragment, R.id.write_email_fragment})
@@ -99,7 +93,7 @@ public class MessageBoxFragment extends BaseFragment {
                 launchFragment(MessageInFragment.class);
                 break;
             case R.id.out_box_fragment:
-//                launchFragment(OutBoxFragment.class);
+                launchFragment(OutBoxFragment.class);
                 break;
             case R.id.write_email_fragment:
 //                launchFragment(WriteEmailFragment.class);
@@ -110,52 +104,52 @@ public class MessageBoxFragment extends BaseFragment {
         }
     }
 
-//    private RestCallback restCallback = new RestCallback<ReceiveBoxResponse>() {
-//        @Override
-//        public boolean onRestComplete(RestRequest request, RestResponse<ReceiveBoxResponse> response) {
-//
-////            if (request.getId() == RECEIVE) {
-////                ReceiveBoxResponse receiveBoxResponse = (ReceiveBoxResponse) (response.getData());
-////                int totalCount =receiveBoxResponse.getList().size();// Integer.parseInt(receiveBoxResponse.getCount());//解决服务端 返回数据 有缓存的 问题
-////                if(totalCount>0){
-////                    totalCount =-1;
-////                }else{
-////                    totalCount =0;
-////                }
-////
-//////                new QBadgeView(getActivity()).bindTarget(in_box_text).setBadgeGravity(Gravity.TOP | Gravity.CENTER).setBadgeNumber(totalCount);
-////
-////                Object  tag=in_box_badge.getTag();
-////
-////                if(tag==null){
-////                    QBadgeView qBadgeView=new QBadgeView(getActivity());
-////                    qBadgeView.bindTarget(in_box_badge);
-////                    qBadgeView.setBadgeGravity(Gravity.START | Gravity.TOP);
-////                    qBadgeView.setBadgeNumber(totalCount);
-////                    in_box_badge.setTag(qBadgeView);
-////                }else{
-////                    QBadgeView qQBadgeView=(QBadgeView)tag;
-////                    qQBadgeView.setBadgeNumber(totalCount);
-////                }
-////            }
-//
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean onRestError(RestRequest request, int errCode, String errDesc) {
-//            if (errCode == 7006) {
-//                CustomDialog dialog = PromptManager.showCustomDialog(getActivity(), "重新登录", errDesc, "重新登录", errCode);
-//                dialog.setCancelable(false);
-//                dialog.show();
-//                return true;
-//            }
-//            return false;
-//        }
-//
-//        @Override
-//        public void onRestStateChanged(RestRequest request, @RestRequest.RestState int state) {
-//        }
-//    };
+    private RestCallback restCallback = new RestCallback<ReceiveBoxResponse>() {
+        @Override
+        public boolean onRestComplete(RestRequest request, RestResponse<ReceiveBoxResponse> response) {
+
+            if (request.getId() == LIST) {
+//                ReceiveBoxResponse receiveBoxResponse = (ReceiveBoxResponse) (response.getData());
+                int totalCount =1;//=receiveBoxResponse.getList().size();// Integer.parseInt(receiveBoxResponse.getCount());//解决服务端 返回数据 有缓存的 问题
+                if(totalCount>0){
+                    totalCount =-1;
+                }else{
+                    totalCount =0;
+                }
+
+//                new QBadgeView(getActivity()).bindTarget(in_box_text).setBadgeGravity(Gravity.TOP | Gravity.CENTER).setBadgeNumber(totalCount);
+
+                Object  tag=in_box_badge.getTag();
+
+                if(tag==null){
+                    QBadgeView qBadgeView=new QBadgeView(getActivity());
+                    qBadgeView.bindTarget(in_box_badge);
+                    qBadgeView.setBadgeGravity(Gravity.START | Gravity.TOP);
+                    qBadgeView.setBadgeNumber(totalCount);
+                    in_box_badge.setTag(qBadgeView);
+                }else{
+                    QBadgeView qQBadgeView=(QBadgeView)tag;
+                    qQBadgeView.setBadgeNumber(totalCount);
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        public boolean onRestError(RestRequest request, int errCode, String errDesc) {
+            if (errCode == 7006) {
+                CustomDialog dialog = PromptManager.showCustomDialog(getActivity(), "重新登录", errDesc, "重新登录", errCode);
+                dialog.setCancelable(false);
+                dialog.show();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onRestStateChanged(RestRequest request, @RestRequest.RestState int state) {
+        }
+    };
 
 }
